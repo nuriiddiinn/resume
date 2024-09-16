a11395ed695479f1fe8f50172ec24fc0f188c3bf
Nuriddin Omonkeldiev <omonkeldiev55@gmail.com>
2024-09-16 18:01:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let username = "Nuriddin Omonkeldiev"
+ username = "Nuriddin Omonkeldiev"
+alert(username)
+
+console.log(username)
+
+let num = 100
+let str = "100"
+
+
+const num1 = 200
+console.log(num-str)
+
+
+
+function sum () {
+    let x = +document.getElementsByName("x")[0].value;
+    let y = +document.getElementsByName("y")[0].value;
+    let z = x + y;
+    let result = document.getElementsByClassName("result")[0];
+    result.innerHTML= "Результат: " + z;
+
+console.log(z)
+
+}
